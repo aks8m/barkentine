7c7b1b5c188a0b6ec6e04642f56ca855ed84b496
Andrew K Sills <andrew.k.sills@gmail.com>
2018-06-30 19:49:02 -0400

    Initial framing/wiring of the tech stack
    Includes most of the tech stack to create the single page application

---
+++ backend/src/main/java/com/github/aks8m/barkentine/BarkentineApplication.java
@@ -0,0 +1,12 @@
+package com.github.aks8m.barkentine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkentineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkentineApplication.class, args);
+	}
+}
