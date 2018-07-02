6af9d37e28d7722f46fb3fcf7d68546c1a2044b7
Andrew K Sills <andrew.k.sills@gmail.com>
2018-07-01 23:42:06 -0400

    Implemented Papa Parse functionality
    Worked on general layout, and got the uploaded file parsing working.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,50 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        name: 'Vue.js',
+        uploadFileLabel: 'Choose .csv file to parse',
+        selectedCSV: '',
+        parsedData: '',
+        showUploadProgressBar: false
+    },
+    // define methods under the `methods` object
+    methods: {
+        onFileChange: function (f) {
+            var vm = this;
+
+            vm.selectedCSV = f.target.files[0];
+            vm.uploadFileLabel = vm.selectedCSV.name;
+        },
+        parseCSV: function () {
+            var vm = this;
+
+            vm.showUploadProgressBar = true;
+            Papa.parse(vm.selectedCSV, {
+                delimiter: "", // auto-detect
+                newline: "", // auto-detect
+                quoteChar: '"',
+                escapeChar: '"',
+                header: true,
+                trimHeaders: false,
+                dynamicTyping: false,
+                preview: 0,
+                encoding: "",
+                worker: false,
+                comments: false,
+                step: undefined,
+                complete: function (results, file) {
+                    vm.parsedData = results;
+                    vm.showUploadProgressBar = false;
+                },
+                error: undefined,
+                download: false,
+                skipEmptyLines: false,
+                chunk: undefined,
+                fastMode: undefined,
+                beforeFirstChunk: undefined,
+                withCredentials: undefined,
+                transform: undefined
+            });
+        }
     }
 });
