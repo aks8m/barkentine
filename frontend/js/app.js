e8faaa8dba9405355ae0b1550afab8aac0066781
Andrew K Sills <andrew.k.sills@gmail.com>
2018-07-03 21:59:17 -0400

    Completed Local Upload (CSV -> JSON) functionality
    Using Papa Parse, the frontend supports Papa Parse's JSON transformation output from a local CSV file.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -4,8 +4,18 @@ var app = new Vue({
         name: 'Vue.js',
         uploadFileLabel: 'Choose .csv file to parse',
         selectedCSV: '',
-        parsedData: '',
-        showUploadProgressBar: false
+        parsedData: [],
+        showUploadProgressBar: false,
+        columnCount: '0',
+        fileSize: '0 MB',
+        rowsParsed: '',
+        personaName: '',
+        rowsParsed: '0',
+        chunkSize: 1024,
+        chunkPercentage: '',
+        loadProgressStyleValue: 'width:0%',
+        chunkCounter: 0,
+        isParseBtnDisabled: false
     },
     // define methods under the `methods` object
     methods: {
@@ -14,37 +24,59 @@ var app = new Vue({
 
             vm.selectedCSV = f.target.files[0];
             vm.uploadFileLabel = vm.selectedCSV.name;
+            vm.personaName = vm.uploadFileLabel.split(".csv")[0];
+            vm.fileSize = (vm.selectedCSV.size / 1000000).toFixed(2) + ' MB';
+            vm.chunkPercentage = vm.chunkSize / parseInt(vm.selectedCSV.size);
+
+            Papa.parse(vm.selectedCSV, {
+                header: true,
+                preview: 1,
+                complete: function (results, file) {
+                    vm.columnCount = Object.keys(results.data[0]).length;
+                }
+            });
         },
         parseCSV: function () {
             var vm = this;
 
-            vm.showUploadProgressBar = true;
-            Papa.parse(vm.selectedCSV, {
-                delimiter: "", // auto-detect
-                newline: "", // auto-detect
-                quoteChar: '"',
-                escapeChar: '"',
-                header: true,
-                trimHeaders: false,
-                dynamicTyping: false,
-                preview: 0,
-                encoding: "",
-                worker: false,
-                comments: false,
-                step: undefined,
-                complete: function (results, file) {
-                    vm.parsedData = results;
-                    vm.showUploadProgressBar = false;
-                },
-                error: undefined,
-                download: false,
-                skipEmptyLines: false,
-                chunk: undefined,
-                fastMode: undefined,
-                beforeFirstChunk: undefined,
-                withCredentials: undefined,
-                transform: undefined
-            });
+            if (vm.selectedCSV != '' && vm.personaName != '') {
+
+                vm.showUploadProgressBar = true;
+
+                Papa.parse(vm.selectedCSV, {
+                    chunkSize: vm.chunkSize,
+                    header: true,
+                    worker: true,
+                    chunk: function (results, parser) {
+                        vm.rowsParsed = (parseInt(vm.rowsParsed) + results.data.length).toString();
+                        vm.chunkCounter += 1;
+                        vm.loadProgressStyleValue = 'width:' +
+                            vm.chunkCounter * vm.chunkPercentage * 100 + '%';
+                    },
+                    complete: function (results, file) {
+                        vm.showUploadProgressBar = false;
+                        vm.loadProgressStyleValue = 'width:0%';
+                        vm.isParseBtnDisabled = true;
+                    }
+                });
+            }
+        },
+        clearUpload: function () {
+            var vm = this;
+
+            vm.uploadFileLabel = 'Choose .csv file to parse';
+            vm.selectedCSV = '';
+            vm.parsedData = [];
+            vm.showUploadProgressBar = false;
+            vm.columnCount = 0;
+            vm.fileSize = '0 MB';
+            vm.rowsParsed = '0';
+            vm.personaName = '';
+            vm.rowsParsed = '0';
+            vm.chunkPercentage = '';
+            vm.loadProgressStyleValue = 'width:0%';
+            vm.chunkCounter = 0;
+            vm.isParseBtnDisabled = false;
         }
     }
 });
